7ea9f41a7bb44844e7928ed7a388aba6d885edfc
YangHaoChen <x73901@live.com>
2021-01-11 15:52:39 +0800

    读表c#工程

---
+++ Tables/Converter_proj/TableConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableConverter
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //Console.WriteLine(System.Environment.CurrentDirectory);
+            //Console.WriteLine( System.Environment.SystemDirectory );
+
+            //1、获取同目录下表路径配置文件（FFTA_Remake/Tables），获取不到则新建，路径指向Tables/Public文件夹
+
+            var localPath = System.Environment.CurrentDirectory;
+            var tableDirectory = localPath + @"\Public";
+            var pathDirectory = localPath + @"\path.txt";
+            //检查目录和创建
+            if (!Directory.Exists(tableDirectory) || !File.Exists( pathDirectory ))
+            {
+                Console.WriteLine(string.Format("不存在目录 {0} ,手动创建...",tableDirectory));
+                Tools.CreateDefaultPathFile( pathDirectory, tableDirectory );
+                Console.WriteLine( "完成" );
+            }
+
+            //2、读Public下的所有Excel并生成tsv之Assets\Res\Config下，原文件直接覆盖
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Tables/Converter_proj/TableConverter/Tools/Tools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableConverter
+{
+    public class Tools
+    {
+        /// <summary>
+        /// 创建默认的路径文件
+        /// </summary>
+        public static void CreateDefaultPathFile (string path, string dicPath)
+        {
+            //目录不存在，先创建目录
+            if (!Directory.Exists( dicPath ))
+                Directory.CreateDirectory( dicPath );
+
+            if (!File.Exists( path ))
+            {
+                FileStream fs = new FileStream( path, FileMode.OpenOrCreate, FileAccess.ReadWrite );
+                StreamWriter sw = new StreamWriter( fs );
+                sw.WriteLine( dicPath );
+                sw.Close();
+                fs.Close();
+            }
+        }
+    }
+}
