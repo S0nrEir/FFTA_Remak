e7339445214f7f6caab8c0752534710f085972bc
YangHaoChen <x73901@live.com>
2021-02-19 15:08:01 +0800

    帧管理器

--- FFTA/Assets/Scripts/Base/Entrance.cs
+++ FFTA/Assets/Scripts/Base/Entrance.cs
@@ -1,4 +1,5 @@
-﻿using AquilaFramework.Common.Define;
+﻿using AquilaFramework.Common;
+using AquilaFramework.Common.Define;
 using UnityEngine;
 
 public class Entrance : MonoBehaviour
@@ -8,5 +9,8 @@ public class Entrance : MonoBehaviour
     {
         //这里是为了避免懒加载获取其中的某些对象时出现“场景内GameObject过多”导致的查找消耗问题
         GlobalInstance.Init();
+
+        //frame init
+        FrameController.I.EnsureInit();
     }
 }
