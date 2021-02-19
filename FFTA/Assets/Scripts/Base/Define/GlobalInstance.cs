1b5476dfe74e06046d533a6e65799112c83f7c9b
YangHaoChen <x73901@live.com>
2021-02-19 14:49:20 +0800

    修改全局单例的获取和生成方式，添加commonTools，log类增加注释

--- FFTA/Assets/Scripts/Base/Define/GlobalInstance.cs
+++ FFTA/Assets/Scripts/Base/Define/GlobalInstance.cs
@@ -14,42 +14,56 @@ namespace AquilaFramework.Common.Define
         public static void Init ()
         {
             //MainCamera
-            var mainCamGo = GameObject.Find("Main_Camera");
-            _mainCamera = mainCamGo.GetComponent<Camera>();
+            //var mainCamGo = GameObject.Find("Main_Camera");
+            //_mainCamera = mainCamGo.GetComponent<Camera>();
 
             //uiCamera
-            var uiCamGo = GameObject.Find("UI_Camra");
-            _uiCamera = uiCamGo.GetComponent<Camera>();
+            //var uiCamGo = GameObject.Find("UI_Camra");
+            //_uiCamera = uiCamGo.GetComponent<Camera>();
+            GenMainCamera();
+            GenUICamera();
 
             //UIRoot
-            var uiGo = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
+            GetUIRoot();
+            GetNormalUiRoot();
+            GetGuideUIRoot();
+            //var uiGo = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
             var guideGo = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "GuideLayer" );
-            var normalGo = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
-            _uiRoot = uiGo.transform;
+            //var normalGo = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
+            //_uiRoot = uiGo.transform;
             _guideUIRoot = guideGo.transform;
-            _normalUIRoot = normalGo.transform;
-
-            //hold all
-            DontDestroyOnLoad
-                (
-                    uiCamGo,
-                    mainCamGo,
-                    uiGo,
-                    guideGo,
-                    normalGo
-                );
+            //_normalUIRoot = normalGo.transform;
         }
 
         /// <summary>
         /// 保留gameObject
         /// </summary>
-        private static void DontDestroyOnLoad (params GameObject[] gos)
-        {
-            foreach (var go in gos)
-                GameObject.DontDestroyOnLoad( go );
-        }
+        //private static void DontDestroyOnLoad (params GameObject[] gos)
+        //{
+        //    foreach (var go in gos)
+        //        GameObject.DontDestroyOnLoad( go );
+        //}
 
         #region camera
+        /// <summary>
+        /// UI相机单例
+        /// </summary>
+        private static Camera GenUICamera ()
+        {
+            if (_uiCamera is null)
+            {
+                var go = GameObject.Find( "UI_Camera" );
+                _uiCamera = go.GetComponent<Camera>();
+                if (_uiCamera is null)
+                {
+                    Log.Error("_uiCamera is null!");
+                    return null;
+                }
+                GameObject.DontDestroyOnLoad( _uiCamera );
+            }
+
+            return _uiCamera;
+        }
 
         /// <summary>
         /// UI相机
@@ -59,18 +73,27 @@ namespace AquilaFramework.Common.Define
         /// <summary>
         /// UI相机
         /// </summary>
-        private static Camera UICamera
+        private static Camera UICamera => _uiCamera ?? GenUICamera();
+
+
+        /// <summary>
+        /// 获取主相机单例
+        /// </summary>
+        private static Camera GenMainCamera ()
         {
-            get
+            if (_mainCamera is null)
             {
-                if (_uiCamera is null)
+                var go = GameObject.Find( "Main_Camera" );
+                _mainCamera = go.GetComponent<Camera>();
+                if (_mainCamera is null)
                 {
-                    var go = GameObject.Find("UI_Camera");
-                    _uiCamera = go.GetComponent<Camera>();
-                    GameObject.DontDestroyOnLoad( _uiCamera );
+                    Log.Error("_mainCamera is null");
+                    return null;
                 }
-                return _uiCamera;
+                GameObject.DontDestroyOnLoad( go );
             }
+
+            return _mainCamera;
         }
 
         /// <summary>
@@ -81,85 +104,89 @@ namespace AquilaFramework.Common.Define
         /// <summary>
         /// 主相机，场景相机
         /// </summary>
-        public static Camera MainCamera
-        {
-            get
-            {
-                if (_mainCamera is null)
-                {
-                    var go = GameObject.Find( "Main_Camera" );
-                    _mainCamera = go.GetComponent<Camera>();
-                    GameObject.DontDestroyOnLoad( go );
-                }
-                return _mainCamera;
-            }
-        }
-
+        public static Camera MainCamera => _mainCamera ?? GenMainCamera();
         #endregion
 
         #region UIRoot
+
+        /// <summary>
+        /// 获取UI节点单例
+        /// </summary>
+        private static Transform GetUIRoot ()
+        {
+            if (_uiRoot is null)
+            {
+                //var roots = GameObject.FindGameObjectsWithTag( "UIRoot" );
+                //#todo查找优化，尽量避免Find
+                var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
+                _uiRoot = go.transform;
+                //_uiRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
+                GameObject.DontDestroyOnLoad( _uiRoot );
+            }
+
+            return _uiRoot;
+        }
+
+        /// <summary>
+        /// UI根节点
+        /// </summary>
         private static Transform _uiRoot = null;
 
         /// <summary>
         /// UI根节点
         /// </summary>
-        public static Transform UIRoot
+        public static Transform UIRoot => _uiRoot ?? GetUIRoot();
+
+        /// <summary>
+        /// 获取通常UI节点
+        /// </summary>
+        private static Transform GetNormalUiRoot ()
         {
-            get
+            if (_normalUIRoot is null)
             {
-                if (_uiRoot is null)
-                {
-                    //var roots = GameObject.FindGameObjectsWithTag( "UIRoot" );
-                    //todo查找优化，尽量避免Find
-                    var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
-                    _uiRoot = go.transform;
-                    //_uiRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "UIRoot" );
-                    GameObject.DontDestroyOnLoad( _uiRoot );
-                }
-                return _uiRoot;
+                var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
+                _normalUIRoot = go.transform;
+                //_normalUIRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
+                //GameObject.DontDestroyOnLoad( _uiRoot );
             }
+
+            return _normalUIRoot;
         }
 
+        /// <summary>
+        /// 通常UI节点
+        /// </summary>
         private static Transform _normalUIRoot = null;
 
         /// <summary>
         /// 通常UI节点
         /// </summary>
-        public static Transform NormalUILayer
+        public static Transform NormalUILayer => _normalUIRoot ?? GetNormalUiRoot();
+
+        /// <summary>
+        /// 获取引导层单例
+        /// </summary>
+        private static Transform GetGuideUIRoot ()
         {
-            get
+            if (_guideUIRoot is null)
             {
-                if (_normalUIRoot is null)
-                {
-                    var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
-                    _normalUIRoot = go.transform;
-                    //_normalUIRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "NormalLayer" );
-                    //GameObject.DontDestroyOnLoad( _uiRoot );
-                }
-                return _normalUIRoot;
+                var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "GuideLayer" );
+                _guideUIRoot = go.transform;
+                //_guideUIRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "GuideLayer" );
+
             }
+            return _guideUIRoot;
         }
 
+        /// <summary>
+        /// 引导层
+        /// </summary>
         private static Transform _guideUIRoot = null;
 
         /// <summary>
         /// 引导层
         /// </summary>
-        public static Transform GuideUILayer
-        {
-            get
-            {
-                if (_guideUIRoot is null)
-                {
-                    var go = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "GuideLayer" );
-                    _guideUIRoot = go.transform;
-                    //_guideUIRoot = CommonTools.FindFromObjects( GameObject.FindGameObjectsWithTag( "UIRoot" ), "GuideLayer" );
-
-                }
-                return _guideUIRoot;
-            }
-        }
-
+        public static Transform GuideUILayer => _guideUIRoot ?? GetGuideUIRoot();
         #endregion
     }
 }
--- FFTA/Assets/Scripts/Tools/Log.cs
+++ FFTA/Assets/Scripts/Tools/Log.cs
@@ -19,21 +19,33 @@ namespace AquilaFramework.Common.Tools
             Debug.Log($"<color=purple>${str}</color>");
         }
 
+        /// <summary>
+        /// 消息
+        /// </summary>
         public static void Info (string str)
         {
             Debug.Log( $"<color=green>${str}</color>" );
         }
 
+        /// <summary>
+        /// 警告
+        /// </summary>
         public static void Warnning (string str)
         {
             Debug.Log( $"<color=yellow>${str}</color>" );
         }
 
+        /// <summary>
+        /// 错误
+        /// </summary>
         public static void Error (string str)
         {
             Debug.Log( $"<color=red>${str}</color>" );
         }
 
+        /// <summary>
+        /// 消息
+        /// </summary>
         public static void Msg (string str)
         {
             Debug.Log( $"<color=white>${str}</color>" );
