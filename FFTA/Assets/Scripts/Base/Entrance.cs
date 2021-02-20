43b090438727aac09160a2401a6ebd36395ef08d
YangHaoChen <x73901@live.com>
2021-02-20 10:05:32 +0800

    AquilaLog打包为DLL

--- FFTA/Assets/Scripts/Base/Entrance.cs
+++ FFTA/Assets/Scripts/Base/Entrance.cs
@@ -1,5 +1,6 @@
 ﻿using AquilaFramework.Common;
 using AquilaFramework.Common.Define;
+using AquilaFramework.Common.Tools;
 using UnityEngine;
 
 public class Entrance : MonoBehaviour
@@ -9,7 +10,7 @@ public class Entrance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GlobalVar.Init( _isOpenLog );
+        Log.SetLogOpen( _isOpenLog );
 
         //这里是为了避免懒加载获取其中的某些对象时出现“场景内GameObject过多”导致的查找消耗问题
         GlobalInstance.Init();
--- FFTA/Assets/Scripts/Tools/Log.cs
+++ FFTA/Assets/Scripts/Tools/Log.cs
@@ -1,63 +1,73 @@
-﻿using AquilaFramework.Common.Define;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using AquilaFramework.Common.Define;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-namespace AquilaFramework.Common.Tools
-{
-    /// <summary>
-    /// Log类
-    /// </summary>
-    public class Log
-    {
-        private static void DoLog (string str)
-        {
-            if (!GlobalVar.IsOpenLog)
-                return;
+//namespace AquilaFramework.Common.Tools
+//{
+//    /// <summary>
+//    /// Log类
+//    /// </summary>
+//    public class Log
+//    {
+//        /// <summary>
+//        /// log开关
+//        /// </summary>
+//        public static bool IsOpenLog { get; private set; } = false;
 
-            Debug.Log( str );
-        }
+//        public static void SetLogOpen (bool b)
+//        {
+//            IsOpenLog = b;
+//        }
 
-        public static void Orange (string str)
-        {
-            DoLog( $"<color=orange>${str}</color>" );
-        }
+//        private static void DoLog (string str)
+//        {
+//            if (!IsOpenLog)
+//                return;
 
-        public static void Purple (string str)
-        {
-            DoLog( $"<color=purple>${str}</color>");
-        }
+//            Debug.Log( str );
+//        }
 
-        /// <summary>
-        /// 消息
-        /// </summary>
-        public static void Info (string str)
-        {
-            DoLog( $"<color=green>${str}</color>" );
-        }
+//        public static void Orange (string str)
+//        {
+//            DoLog( $"<color=orange>${str}</color>" );
+//        }
 
-        /// <summary>
-        /// 警告
-        /// </summary>
-        public static void Warnning (string str)
-        {
-            DoLog( $"<color=yellow>${str}</color>" );
-        }
+//        public static void Purple (string str)
+//        {
+//            DoLog( $"<color=purple>${str}</color>");
+//        }
 
-        /// <summary>
-        /// 错误
-        /// </summary>
-        public static void Error (string str)
-        {
-            DoLog( $"<color=red>${str}</color>" );
-        }
+//        /// <summary>
+//        /// 消息
+//        /// </summary>
+//        public static void Info (string str)
+//        {
+//            DoLog( $"<color=green>${str}</color>" );
+//        }
 
-        /// <summary>
-        /// 消息
-        /// </summary>
-        public static void Msg (string str)
-        {
-            DoLog( $"<color=white>${str}</color>" );
-        }
-    }
-}
+//        /// <summary>
+//        /// 警告
+//        /// </summary>
+//        public static void Warnning (string str)
+//        {
+//            DoLog( $"<color=yellow>${str}</color>" );
+//        }
+
+//        /// <summary>
+//        /// 错误
+//        /// </summary>
+//        public static void Error (string str)
+//        {
+//            DoLog( $"<color=red>${str}</color>" );
+//        }
+
+//        /// <summary>
+//        /// 消息
+//        /// </summary>
+//        public static void Msg (string str)
+//        {
+//            DoLog( $"<color=white>${str}</color>" );
+//        }
+//    }
+//}
