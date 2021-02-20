93642fc94c52c54bba1934e1a25d2ee0a90ccd57
YangHaoChen <x73901@live.com>
2021-02-20 17:25:49 +0800

    添加表的.cs生成功能

---
+++ FFTA/Assets/Editor/TableGenerator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class TableGenerator
+{
+    [MenuItem("AquilaFramework/Table/Gen")]
+    /// <summary>
+    /// 生成表对应的CS文件
+    /// </summary>
+    public static void GenTable ()
+    {
+
+    }
+}
