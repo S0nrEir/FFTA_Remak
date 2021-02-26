0d16ce1c7119816696711d82e5c2cfa9ac035060
YangHaoChen <x73901@live.com>
2021-02-26 12:10:16 +0800

    Editor文件

---
+++ FFTA/Assets/Editor/WindowsBase/WindowEditor.cs
@@ -0,0 +1,29 @@
+﻿using AquilaFramework.Common.UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace AquilaFramework.EditorExtension
+{
+    /// <summary>
+    /// WindowsBase编辑类
+    /// </summary>
+    [CustomEditor( typeof( WindowBase ) )]//指定当WindowBase的GameObject选中时执行以下编辑器脚本，
+    public class WindowBaseEditor : Editor
+    {
+        //字段引用-下拉类型
+        private GameObject[] _fieldsArr;
+
+
+        public override void OnInspectorGUI ()
+        {
+            //base.OnInspectorGUI();
+            //GUILayout.Space( 10 );
+
+            //(GameObject)EditorGUI.ObjectField( new Rect( 3, 3,, 20 ), "Find Dependency", obj, typeof( GameObject ) );
+
+            //var option = new GUILayoutOption();
+
+            //_fieldsArr = (GameObject) EditorGUILayout.ObjectField("fields", _fieldsArr, true,);
+        }
+    }
+}
