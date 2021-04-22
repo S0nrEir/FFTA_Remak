369ae2ff5107fe408b1199d03677205e6dba6e16
YangHaoChen <x73901@live.com>
2021-04-22 14:55:04 +0800

    1

--- FFTA/Assets/Scripts/Base/Entrance.cs
+++ FFTA/Assets/Scripts/Base/Entrance.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using AquilaFramework.Common;
+using AquilaFramework.Common.Define;
 
 public class Entrance : MonoBehaviour
 {
@@ -12,11 +14,7 @@ public class Entrance : MonoBehaviour
 
         //这里是为了避免懒加载获取其中的某些对象时出现“场景内GameObject过多”导致的查找消耗问题
         AquilaFramework.Common.Define.GlobalInstance.Init();
-
-        //frame init
-        AquilaFramework.Common.FrameController.I.EnsureInit();
-
-        //objectPoolInit
+        FrameController.I.EnsureInit();
         AquilaFramework.ObjectPool.ObjectPoolMgr.I.Init();
 
         DontDestroyOnLoad( gameObject );
